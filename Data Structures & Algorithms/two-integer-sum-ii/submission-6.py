bf35002a1de6c46e43660323ca6fc448965f7b4d
Larry Tang <93503365+smeleton@users.noreply.github.com>
2026-06-11 21:01:09 -0700

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Brute force time: O(n^2) Space: O(n)
+        # for i in range(len(numbers)):
+        #     for j in range(i + 1, len(numbers)):
+        #         if numbers[i] + numbers[j] == target:
+        #             return [i+1, j+1]
+        l = 0
+        r = len(numbers) - 1
+        while l < r: 
+            while numbers[l] + numbers[r] > target:
+                r -= 1
+            while numbers[l] + numbers[r] < target:
+                l += 1
+            while numbers[l] + numbers[r] == target:
+                return [l+1, r+1]
+        
+        #hash method: requires O(n) space complexity but problem requires O(1)
+        mp = {}
+        for i in range(len(numbers)):
+            diff = target - numbers[i]
+            if diff in mp:
+                return [mp[diff] + 1, i+1]
+            mp[numbers[i]] = i
+        return []
+
+                    
+                
+        
